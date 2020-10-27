a5ea5e2e3feff076bbe19d8f5636a26d265b2335
EslamKhder <eslamkhder81@gmail.com>
2020-10-27 11:19:23 +0200

    first commit

---
+++ back-end/friend-finder/src/main/java/com/spring/frindfinder/FriendFinderApplication.java
@@ -0,0 +1,13 @@
+package com.spring.frindfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendFinderApplication.class, args);
+    }
+
+}
---
+++ back-end/friend-finder/src/test/java/com/spring/frindfinder/FriendFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.frindfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
