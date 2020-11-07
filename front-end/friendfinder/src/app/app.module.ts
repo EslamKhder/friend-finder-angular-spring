e29e0d4818f40b91ef3e073e4b9d6e2a7c40da7d
EslamKhder <eslamkhder81@gmail.com>
2020-11-07 04:57:30 +0200

    create components SignUp and Contact Closes #11

--- front-end/friendfinder/src/app/app.module.ts
+++ front-end/friendfinder/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componants/header/header.component';
 import { FooterComponent } from './componants/footer/footer.component';
+import { SignUpComponent } from './componants/sign-up/sign-up.component';
+import { ContactComponent } from './componants/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SignUpComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
