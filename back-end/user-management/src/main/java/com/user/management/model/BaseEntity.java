1a0af4030f792d916f14b6d929e4e45937aecd88
eslam khder <eslamkhder11@gmail.com>
2022-11-27 14:58:29 +0200

    add lombok

--- back-end/user-management/src/main/java/com/user/management/model/BaseEntity.java
+++ back-end/user-management/src/main/java/com/user/management/model/BaseEntity.java
@@ -1,7 +1,10 @@
 package com.user.management.model;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public abstract class BaseEntity {
 
     private Long id;
@@ -13,44 +16,4 @@ public abstract class BaseEntity {
     private LocalDateTime modifiedTime;
 
     private Long modifiedBy;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public LocalDateTime getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(LocalDateTime createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public LocalDateTime getModifiedTime() {
-        return modifiedTime;
-    }
-
-    public void setModifiedTime(LocalDateTime modifiedTime) {
-        this.modifiedTime = modifiedTime;
-    }
-
-    public Long getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(Long modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
 }
