12f3b6755b2a00453ee8b51ac6186382ff9b55e3
eslam khder <eslamkhder11@gmail.com>
2022-11-27 16:37:46 +0200

    set annotation (BaseEntity)

--- back-end/user-management/src/main/java/com/user/management/model/BaseEntity.java
+++ back-end/user-management/src/main/java/com/user/management/model/BaseEntity.java
@@ -2,18 +2,37 @@ package com.user.management.model;
 
 import lombok.Data;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Data
-public abstract class BaseEntity {
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+@Data
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    @Basic(optional = false)
     private Long id;
 
     private boolean active;
 
+    @CreatedDate
+    @Column(name = "created_time", updatable = false)
     private LocalDateTime createdTime;
 
+    @LastModifiedDate
+    @Column(name = "modified_time")
     private LocalDateTime modifiedTime;
 
-    private Long modifiedBy;
+    // private Long modifiedBy;
 }
