106156a4c9f05979fe17609d909dc8bb62232199
Your Name <you@example.com>
2023-06-03 13:01:36 +0300

    Create AccessTokenOrganizationHandler

--- back-end/user-management/src/main/java/com/user/management/config/jwt/AccessTokenOrganizationHandler.java
+++ back-end/user-management/src/main/java/com/user/management/config/jwt/AccessTokenOrganizationHandler.java
@@ -1,5 +1,6 @@
 package com.user.management.config.jwt;
 
+import com.user.management.model.organization.Organization;
 import com.user.management.model.user.User;
 import com.user.management.sittings.Configurations;
 import io.jsonwebtoken.JwtBuilder;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 
 @Component
-public class AccessTokenUserHandler extends TokenHandler<User> {
+public class AccessTokenOrganizationHandler extends TokenHandler<Organization> {
 
     private Duration accessTokenTtl;
 
@@ -19,22 +20,22 @@ public class AccessTokenUserHandler extends TokenHandler<User> {
      *
      * @param configurations
      */
-    public AccessTokenUserHandler(Configurations configurations) {
+    public AccessTokenOrganizationHandler(Configurations configurations) {
         super(configurations);
         this.accessTokenTtl = configurations.getToken().getAccessTokenTime();
         this.refreshTokenTtl = configurations.getToken().getRefreshTokenTime();
     }
 
     @Override
-    public String createToken(User user) {
-        JwtBuilder tokenBuilder = createToken(user.getId().toString(), accessTokenTtl);
-        tokenBuilder.claim("loginName", user.getName());
-        tokenBuilder.claim("scope", user.getScope());
+    public String createToken(Organization organization) {
+        JwtBuilder tokenBuilder = createToken(organization.getId().toString(), accessTokenTtl);
+        tokenBuilder.claim("referenceId", organization.getReferenceId());
+        tokenBuilder.claim("scope", organization.getScope());
         return tokenBuilder.compact();
     }
 
-    public String createRefreshToken(User user) {
-        return createToken(user.getId().toString(), accessTokenTtl).compact();
+    public String createRefreshToken(Organization organization) {
+        return createToken(organization.getId().toString(), accessTokenTtl).compact();
     }
 
 }
