1d9b654c29ca342c287f90b61096a2d04686fc27
Eslam.khder <eslamkhder11@gmail.com>
2024-06-28 23:00:48 +0300

    CREATE PROCEDURE ADD_USER_OR_ORG_FROM_USER_MANAGEMENT

---
+++ back-end/friend-finder/src/main/resources/db/changelog/procedure/sql/2024_06_28_00_ADD_USER_OR_ORG_FROM_USER_MANAGEMENT.sql
@@ -0,0 +1,13 @@
+DROP PROCEDURE ADD_USER_OR_ORG_FROM_USER_MANAGEMENT;
+CREATE PROCEDURE ADD_USER_OR_ORG_FROM_USER_MANAGEMENT
+    @USER_ID INT,
+    @SCOPE VARCHAR(50)
+AS
+BEGIN
+    INSERT INTO User_System (active
+                            ,created_time
+                            ,modified_time
+                            ,scope
+                            ,user_id)
+    VALUES (1, GETDATE(), GETDATE(), @SCOPE, @USER_ID)
+END;
